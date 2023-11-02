39434ebf842efeb67714378b78bc89ce30690f97
Marcelo Palma <marcelobaltazarpalma@gmail.com>
2023-11-02 11:23:06 -0300

    ds

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,19 +1,11 @@
-"use client"
-
-import React from 'react'
-import GithubIcon from '../../../public/github-icon.svg'
-import LinkedinIcon from '../../../public/linkedin-icon.svg'
-import TwitterIcon from '../../../public/x-twitter-icon.svg'
-import Link from 'next/link'
-import Image from 'next/image'
-import { useState } from 'react'
-
-
-
-
+"use client";
+import React, { useState } from "react";
+import GithubIcon from "../../../public/github-icon.svg";
+import LinkedinIcon from "../../../public/linkedin-icon.svg";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function EmailSection() {
-
     const [emailSubmitted, setEmailSubmitted] = useState(false);
 
     const handleSubmit = async (e) => {
@@ -47,68 +39,73 @@ export default function EmailSection() {
         }
     };
 
-
-
     return (
-        <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4'>
-            <article>
-                <h5 className='text-xl font-bold text-white my-2'>
-                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-red-600 z-index-100">
-                        Let's Connect
+        <section
+            id="contact"
+            className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
+        >
+            <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
+            <div className="z-10">
+                <h5 className="text-xl font-bold my-2">
+                    <span className="bg-clip-text bg-gradient-to-r from-blue-400 to-red-600 text-transparent">
+                        Let&apos;s Connect
                     </span>
                 </h5>
-                <p className='text-[#ADB7BE] mb-4 max-w-md'>
-                    {""}
-                    I'm currently looking for new opportunities, my inbox is always open.
-                    Whether you have a question or just want to say hi, I'll try my best
-                    to get back to you!
+                <p className="text-[#ADB7BE] mb-4 max-w-md">
+                    {" "}
+                    I&apos;m currently looking for new opportunities, my inbox is always
+                    open. Whether you have a question or just want to say hi, I&apos;ll
+                    try my best to get back to you!
                 </p>
-                <ul className='socials flex flex-row gap-3'>
-                    <Link href="https://github.com/MarcePalma">
-                        <Image src={GithubIcon} alt='Github Icon' />
+                <div className="socials flex flex-row gap-2">
+                    <Link href="github.com">
+                        <Image src={GithubIcon} alt="Github Icon" />
                     </Link>
-                    <Link href="https://www.linkedin.com/in/marcelo-palma-6507b926a">
-                        <Image src={LinkedinIcon} alt='Linkedin Icon' />
+                    <Link href="linkedin.com">
+                        <Image src={LinkedinIcon} alt="Linkedin Icon" />
                     </Link>
-                    <Link href="https://twitter.com/SeyyTT">
-                        <Image src={TwitterIcon} alt='Twitter Icon' />
-                    </Link>
-                </ul>
-            </article>
-            <article>
+                </div>
+            </div>
+            <div>
                 {emailSubmitted ? (
                     <p className="text-green-500 text-sm mt-2">
                         Email sent successfully!
                     </p>
                 ) : (
                     <form className="flex flex-col" onSubmit={handleSubmit}>
-                        <div className='mb-6'>
+                        <div className="mb-6">
                             <label
                                 htmlFor="email"
-                                className='text-white block text-sm font-medium'>
-                                Your Email
+                                className="text-white block mb-2 text-sm font-medium"
+                            >
+                                Your email
                             </label>
                             <input
+                                name="email"
                                 type="email"
-                                id='email'
+                                id="email"
                                 required
-                                className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
-                                placeholder='example@gmail.com' />
+                                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                                placeholder="jacob@google.com"
+                            />
                         </div>
-                        <div className='mb-6'>
+                        <div className="mb-6">
                             <label
                                 htmlFor="subject"
-                                className='text-white block text-sm font-medium'>
+                                className="text-white block text-sm mb-2 font-medium"
+                            >
                                 Subject
                             </label>
                             <input
-                                type="subject"
-                                id='subject'
+                                name="subject"
+                                type="text"
+                                id="subject"
                                 required
-                                className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
-                                placeholder='<Just Say Hi!/>' />
+                                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                                placeholder="Just saying hi"
+                            />
                         </div>
-                        <div className='mb-6'>
+                        <div className="mb-6">
                             <label
                                 htmlFor="message"
                                 className="text-white block text-sm mb-2 font-medium"
@@ -123,14 +120,15 @@ export default function EmailSection() {
                             />
                         </div>
                         <button
-                            type='submit'
-                            className='bg-purple-500 hover:bg-purple-800 text-white font-medium py-2.5 px-5 rounded-lg w-full'
+                            type="submit"
+                            className="bg-gradient-to=r from-blue-400 to-red-600 hover:bg-red-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
                         >
-                            Send Message!
+                            Send Message
                         </button>
                     </form>
                 )}
-            </article>
+            </div>
         </section>
-    )
-}
+    );
+};
+
--- src/app/components/achievementsection.jsx
+++ src/app/components/achievementsection.jsx
@@ -23,7 +23,7 @@ const achievementsList = [
 
 export default function AchievementsSection() {
   return (
-    <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+    <div className="py-8 px-4 xl:gap-10 sm:py-16 xl:px-16">
       <div className="sm:border-[#a0708a] sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
         {achievementsList.map((achievement, index) => {
           return (
