7bf983574dee92cd0aee6d9a1581d02e27a8a751
Marcelo Palma <marcelobaltazarpalma@gmail.com>
2023-11-02 11:31:30 -0300

    a

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Link from 'next/link'
 import NavLink from './NavLink.jsx'
 import { useState } from 'react'
-import {Bars3Icon, XmarkIcon} from '@heroicons/react/24/solid'
+import {Bars3Icon } from '@heroicons/react/24/solid'
 import MenuOverlay from './MenuOverlay'
 
 const navLinks = [
