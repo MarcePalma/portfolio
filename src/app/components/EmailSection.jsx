3d1216d1786097eaf4474d7006f7364ecd082466
Marcelo Palma <marcelobaltazarpalma@gmail.com>
2023-11-10 13:44:13 -0300

    add Link to Github and Linkedin

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -58,10 +58,10 @@ export default function EmailSection() {
                     try my best to get back to you!
                 </p>
                 <div className="socials flex flex-row gap-2">
-                    <Link href="github.com">
+                    <Link href="https://github.com/MarcePalma">
                         <Image src={GithubIcon} alt="Github Icon" />
                     </Link>
-                    <Link href="linkedin.com">
+                    <Link href="https://www.linkedin.com/in/marcelo-palma-6507b926a/">
                         <Image src={LinkedinIcon} alt="Linkedin Icon" />
                     </Link>
                 </div>
